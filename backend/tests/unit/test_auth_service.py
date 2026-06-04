5a863f541c217569cb41a8717e88af44edcdfe03
RodionKholodaev <kholodaev10@gmail.com>
2026-06-04 15:26:23 +0300

    добавил еще один тест просто попрактиковался

--- backend/tests/unit/test_auth_service.py
+++ backend/tests/unit/test_auth_service.py
@@ -1,8 +1,8 @@
 import pytest
 from src.services.auth_service import AuthService
-from src.schemas.user import RegisterRequest
+from src.schemas.user import RegisterRequest, LoginRequest
 from tests.fakes.test_user_repository import FakeUserRepository
-
+from src.core.exceptions import UnauthorizedError  
 
 @pytest.mark.asyncio
 async def test_register():
@@ -19,3 +19,17 @@ async def test_register():
 
     assert result.user_id == 1
     assert result.username == "test"
+
+@pytest.mark.asyncio
+async def test_login():
+    repo = FakeUserRepository()
+    service = AuthService(repo) #type: ignore
+    body = LoginRequest(
+        email="thereisnoemaillikethat@mail.ru",
+        password="qwerty"
+    )
+
+    with pytest.raises(UnauthorizedError) as exc_info:
+        result = await service.login(body)
+    
+    assert str(exc_info.value) == "Неверный email или пароль"
