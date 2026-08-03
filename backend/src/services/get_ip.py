dfb01066e9b78dc1b8c7726330a681e9f94082eb
RodionKholodaev <kholodaev10@gmail.com>
2026-08-03 22:27:23 +0300

    логи для get_ip

--- backend/src/services/get_ip.py
+++ backend/src/services/get_ip.py
@@ -1,5 +1,9 @@
+import logging
 from fastapi import Request
 import ipaddress as ip
+
+logger = logging.getLogger(__name__)
+
 
 def get_ip(request: Request):
     # получаем ip из заголовка nginx
@@ -7,11 +11,21 @@ def get_ip(request: Request):
     if forwarded_for:
         # если он есть, то есть nginx
         client_ip = ip.ip_address(forwarded_for.split(",")[0].strip())
+        logger.debug(
+            "Client IP resolved from X-Forwarded-For header",
+            extra={"client_ip": str(client_ip), "header": forwarded_for},
+        )
     else:
         # если nginx нет
         client_ip = ip.ip_address(request.client.host)  # type: ignore
+        logger.debug(
+            "Client IP resolved from direct connection",
+            extra={"client_ip": str(client_ip)},
+        )
 
     return client_ip
+
+
 """
 важно: в настройках nginx нужно убирать заголовок X-Forwarded-For из запросов!!!
 """
