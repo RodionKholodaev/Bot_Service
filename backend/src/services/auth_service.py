dc675a63952ecd76350963eaf22d62d8120fa97a
RodionKholodaev <kholodaev10@gmail.com>
2026-08-03 14:16:04 +0300

    Расставил логи в auth_service, решил писать их на английском. Думаю это самый правильный подход к такому делу

--- backend/src/services/auth_service.py
+++ backend/src/services/auth_service.py
@@ -4,13 +4,24 @@ from src.core.security import hash_password, verify_password
 from src.schemas.user import TokenResponse
 from src.core.security import create_token
 from src.core.exceptions import ConflictError, UnauthorizedError
+
+import logging
+logger = logging.getLogger(__name__)
+
 class AuthService:
     def __init__(self, repo: UserRepository):
         self.repo = repo
     
     async def register(self, body):
         if await self.repo.email_exists(body.email):
+            logger.warning(
+                "Registration failed: email already exists",
+                extra={
+                    "email": body.email
+                }
+            )
             raise ConflictError("Email уже занят")
+        
 
 
         user = User(
@@ -19,6 +30,14 @@ class AuthService:
             password_hash=hash_password(body.password),
         )
         await self.repo.create_user(user)
+
+        logger.info(
+            "User registered",
+            extra={
+                "user_id": user.id,
+                "email": user.email,
+            },
+        )
         
 
         return TokenResponse(
@@ -30,7 +49,21 @@ class AuthService:
     async def login(self, body):
         user = await self.repo.get_user(body.email)
         if not user or not verify_password(body.password, user.password_hash):
+            logger.warning(
+                "Login failed",
+                extra={
+                    "email":body.email,
+                },
+            )
+
             raise UnauthorizedError("Неверный email или пароль")
+
+        logger.info(
+            "User logged in",
+            extra={
+                "user_id":user.id,
+            },
+        )
         
         return TokenResponse(
             access_token=create_token(user.id),
