7794d472929a24cd656d4bbc7bfe1d92fed27eb0
RodionKholodaev <kholodaev10@gmail.com>
2026-08-13 17:13:43 +0300

    Расствавил коментарии в конфиге для логирования (чутка) и сделал так чтобы новые шрифты брались из библиатеки а не с сайта гугла (туда нет доступа из-за блокировок (кажется))

--- backend/src/logger_config.py
+++ backend/src/logger_config.py
@@ -1,7 +1,8 @@
 import logging
+# режет логи по размеру
 from logging.handlers import RotatingFileHandler
 from pathlib import Path
-
+# путь к папке с логами
 LOG_DIR = Path(__file__).resolve().parent.parent / "logs"
 
 # Набор стандартных полей LogRecord — всё, чего нет в этом наборе, пришло через extra={...}.
@@ -31,21 +32,23 @@ class ExtraFormatter(logging.Formatter):
 
 
 def setup_logging():
+    # формат вывода логов
     formatter = ExtraFormatter(
         fmt='%(levelname)s:     %(asctime)s - [%(filename)s:%(lineno)d] - %(message)s',
         datefmt='%Y-%m-%d %H:%M:%S',
     )
-
+    # создаем файл для записи логов
     LOG_DIR.mkdir(parents=True, exist_ok=True)
 
     console_handler = logging.StreamHandler()
     console_handler.setFormatter(formatter)
-
+    
+    # настройки сохранения в файл
     file_handler = RotatingFileHandler(
         LOG_DIR / "app.log", maxBytes=10 * 1024 * 1024, backupCount=5, encoding="utf-8"
     )
     file_handler.setFormatter(formatter)
-
+    # какой уровень показывать, какие хендлеры использовать
     logging.basicConfig(level=logging.INFO, handlers=[console_handler, file_handler])
 
 
